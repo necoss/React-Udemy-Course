3cb84322903817394c1d23c1f6cbcae929519c35
trexi <vartypeez@gmail.com>
2025-02-16 20:10:09 +0300

    Completed Lesson 08-reusable-components

---
+++ 08-reusable-components/src/App.js
@@ -0,0 +1,14 @@
+import './App.css';
+import MyComponent from './components/MyComponent';
+import TextComponent from './components/TextComponent';
+
+function App() {
+  return (
+    <div className="App">
+      <MyComponent />
+      <TextComponent />
+    </div>
+  );
+}
+
+export default App;
---
+++ 08-reusable-components/src/components/TextComponent.js
@@ -0,0 +1,9 @@
+function TextComponent() {
+	return (
+		<div>
+			<p>Example for text component aka test</p>
+		</div>
+	)
+}
+
+export default TextComponent
